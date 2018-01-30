57960d3f6d381f8ceeabd9989ccc131f9e0a0003
Марат Якушев <yakushev1396@gmail.com>
2018-01-30 23:05:46 +0300

    Сделал Таймер, добавил кнопку паузы

--- Assets/scripts/GameTimer.cs
+++ Assets/scripts/GameTimer.cs
@@ -7,31 +7,50 @@ using UnityEngine.UI;
 public class GameTimer : MonoBehaviour {
 
     public Text timerText;
-    public float startTime;
 
+
+    public float start_seconds;
     public bool isTimerWork = false;
 
     public void StartTime()
     {
-
+        start_seconds++;
+        isTimerWork = true;
         StartCoroutine(Timer());
+        Debug.Log("Maaath " + Mathf.Floor(9.6f));
+    }
+
+    public void PauseTimer()
+    {
+        if (isTimerWork)
+        {
+            isTimerWork = false;
+            Debug.Log("Time " + start_seconds);
+        }
+        else
+        {
+            StartTime();
+        }
+
     }
 
     private IEnumerator Timer()
-    {
-        isTimerWork = true;
+    {     
         while (isTimerWork)
         {
-            if (startTime > 0)
+            if (start_seconds > 0)
             {
-                startTime--;
-                timerText.text = "0:"+startTime.ToString("f0");
+                start_seconds--;
+                float minutes = Mathf.Floor(start_seconds / 60);
+                float seconds = start_seconds % 60;               
+                timerText.text = (minutes.ToString("0") + ":"+ seconds.ToString("00"));
                 yield return new WaitForSeconds(1);
             }
             else
             {
                 isTimerWork = false;
                 Debug.Log("isDone!");
+                yield return null;
             }
         }
     }
